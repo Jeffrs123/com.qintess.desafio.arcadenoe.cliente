5c32b39e1ff655d961ed78653bb629c9fcd02b2c
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-15 13:14:11 -0300

    atualizações para fazer o deploy no Heroku - 5

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.arcadenoecliente;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/arcadenoecliente`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/arcadenoecliente/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
