ba682c76a8a02f5dd818e245bbb8107dc39f00c7
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-18 14:17:03 -0300

    env - att 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'https://arcadenoe.herokuapp.com/',
+  API: 'https://arcadenoe.herokuapp.com/api/v1/',
   API2: 'http://localhost:8080/api/v1/'
 };
 
