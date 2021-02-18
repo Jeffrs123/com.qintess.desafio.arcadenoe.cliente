9bccce20ba69e705b7f550f653c7f25068abc2b0
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-18 17:41:14 -0300

    att

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'https://arcadenoe.herokuapp.com/api/v1/',
+  API: 'https://clinicaveterinariaarcadenoe.herokuapp.com/api/v1/',
   API2: 'http://localhost:8080/api/v1/'
 };
